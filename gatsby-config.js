cc4fa3452b4a6956e4c04aa1ab98e7717cacf69e
jasonpkovalski <jasonpkovalski@gmail.com>
2021-12-22 18:22:57 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "JPK Landing Site",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-154161137-1",
+      },
+    },
+  ],
+};
